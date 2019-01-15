3480f4183d27db10c145f0fc2c8c600cc21fb68f
ryanwardle <wardle2106@gmail.com>
2019-01-15 15:44:36 -0500

    update

--- dictionary-project/src/app/get-data.service.ts
+++ dictionary-project/src/app/get-data.service.ts
@@ -6,6 +6,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GetDataService {
   definition;
+  sentence;
+  origin;
+  partOfSpeech;
 
   constructor(private http: HttpClient) { }
 
@@ -17,6 +20,12 @@ export class GetDataService {
         console.log(data);
         this.definition = data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0];
         console.log(this.definition);
+        // this.sentence = data.results;
+        // console.log(this.sentence);
+        this.partOfSpeech = data.results[0].lexicalEntries[0].lexicalCategory;
+        console.log(this.partOfSpeech);
+        this.origin = data.results[0].lexicalEntries[0].entries[0].etymologies[0];
+        console.log(this.origin);
       });
   }
 }
--- dictionary-project/src/app/word.model.ts
+++ dictionary-project/src/app/word.model.ts
@@ -1,6 +1,7 @@
 export class Word {
 
-  constructor(public word: string, public definition: string, public partOfSpeech: string, public synonyms: string[]) {
+  constructor(public word: string, public definition: string, public partOfSpeech: string,
+     public orgin: string, public synonyms: string[]) {
 
   }
 }
