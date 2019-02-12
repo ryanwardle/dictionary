c9fb5aa87c35700d9ed2951b19590ea282094608
ryanwardle <wardle2106@gmail.com>
2019-02-12 16:39:16 -0500

    update

--- dictionary-project/src/app/get-data.service.ts
+++ dictionary-project/src/app/get-data.service.ts
@@ -1,17 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Word } from './word.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetDataService {
-  definition;
-  sentence;
-  origin;
-  partOfSpeech;
-  synonyms;
-  // returnWord: Word;
 
   constructor(private http: HttpClient) { }
 
@@ -24,26 +17,9 @@ export class GetDataService {
     });
   }
 
-  getData(word: string) {
-    // const url = /api/v1/entries/en/;
+  getData(word: string): any {
 
     // Eventualyy will have to handle words that have more than one meaning
-      return this.http.get(`/api/v1/entries/en/${word}`)
-      .subscribe((data: any) => {
-        // console.log(data);
-        this.definition = data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0];
-        // return this.definition;
-        // console.log(this.definition);
-        // this.sentence = data.results;
-        // console.log(this.sentence);
-        this.partOfSpeech = data.results[0].lexicalEntries[0].lexicalCategory;
-        // console.log(this.partOfSpeech);
-        this.origin = data.results[0].lexicalEntries[0].entries[0].etymologies[0];
-        // console.log(this.origin);
-        this.synonyms = ['synonym-1', 'synonym-2'];
-        console.log(new Word(word, this.definition, this.partOfSpeech, this.origin, this.synonyms));
-        return new Word(word, this.definition, this.partOfSpeech, this.origin, this.synonyms);
-
-      });
+      return this.http.get(`/api/v1/entries/en/${word}`);
   }
 }
--- dictionary-project/src/app/search/search.component.ts
+++ dictionary-project/src/app/search/search.component.ts
@@ -9,6 +9,11 @@ import { GetDataService } from '../get-data.service';
 })
 export class SearchComponent implements OnInit {
   wordData: any;
+  definition;
+  origin;
+  partOfSpeech;
+  synonyms;
+  word;
 
   constructor(private retrieveData: GetDataService) { }
 
@@ -16,8 +21,15 @@ export class SearchComponent implements OnInit {
   }
 
   onSearchWord(event: any) {
-    // console.log(event);
-    this.wordData = this.retrieveData.getData(event.target.previousSibling.value);
-    console.log(this.wordData);
+      this.wordData = this.retrieveData.getData(event.target.previousSibling.value);
+
+// CREATING A NEW WORD, BASED ON RETURNED DATA FROM API CALL
+      this.wordData.subscribe((data: any) => {
+      this.definition = data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0];
+      this.partOfSpeech = data.results[0].lexicalEntries[0].lexicalCategory;
+      this.origin = data.results[0].lexicalEntries[0].entries[0].etymologies[0];
+      this.synonyms = ['synonym-1', 'synonym-2'];
+      this.word = event.target.previousSibling.value;
+    });
   }
 }
