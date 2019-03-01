d55c6ad82bda81460cf39ad76d6b8405388eb59a
ryanwardle <wardle2106@gmail.com>
2019-03-01 16:37:38 -0500

    update

--- dictionary-project/src/app/get-data.service.ts
+++ dictionary-project/src/app/get-data.service.ts
@@ -9,20 +9,7 @@ export class GetDataService {
   constructor(private http: HttpClient) { }
 
   getRandomWord() {
-
-    // WILL NEED TO FIGURE THE CORRECT PATH AND THEN FILTER TO GET ONLY ONE WORD...AT RANDOM;
-    const lexicalCategories = ['noun', 'verb', 'adjective', 'adverb'];
-    const lexicalCategoryNumber = Math.floor(Math.random() * lexicalCategories.length);
-    const lexicalCategory = lexicalCategories[lexicalCategoryNumber];
-
-// NEED TO FIGURE OUT DOAMINS
-    const domains = ['music', 'politics', 'medicine'];
-    const domainsNumber = Math.floor(Math.random() * domains.length);
-    const domain = domains[domainsNumber];
-
-    // GOING TO NEED TO THINK OF MORE FILTERS TO GET THE LIST DOWN AND USE ALL 26 LETTERS
-    // MAYBE RANDOM PARTOFSPEECH, RANDOM LETTER, RANDOM LENGTH ETC...
-    return this.http.get(`/api/v1/wordlist/en/lexicalCategory=${lexicalCategory};regions=us;domains=${domain}`);
+    return this.http.get(`wordnikApi/v4/words.json/randomWord`);
   }
 
   getCorrectForm(word: string) {
@@ -30,7 +17,6 @@ export class GetDataService {
   }
 
   getData(word: string): any {
-
     // Eventualyy will have to handle words that have more than one meaning
       return this.http.get(`/api/v1/entries/en/${word}/regions=us`);
   }
--- dictionary-project/src/app/home/home/home.component.ts
+++ dictionary-project/src/app/home/home/home.component.ts
@@ -25,27 +25,7 @@ export class HomeComponent implements OnInit {
   test() {
     this.getData.getRandomWord()
     .subscribe((data: any) => {
-
-//   CREATE RANDOM NUMBER TO GET BASED ON ARRAY RETURNED FROM WORDLIST CALL
-      const randomNumber = Math.floor(Math.random() * data.results.length);
-      this.randomWord = data.results[randomNumber].word;
       console.log(data);
-      console.log(this.randomWord);
-
-      // this.getData.getData(this.randomWord)
-      // .subscribe(randomWordData => {
-      //   this.definition = correctedWordData.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0];
-      //   this.partOfSpeech = correctedWordData.results[0].lexicalEntries[0].lexicalCategory;
-      //   this.origin = correctedWordData.results[0].lexicalEntries[0].entries[0].etymologies[0];
-      //
-      //   // NEED TO GET SYNONYMS AND THEN MAKE CLICKABLE SO THAT YOU CAN THEN SEARCH THAT WORD
-      //   this.synonyms = ['synonym-1', 'synonym-2'];
-      //
-      //   this.word = this.correctWord.toLowerCase();
-      //
-      //   // CREATING A NEW WORD, BASED ON RETURNED DATA FROM API CALL
-      //   this.searchResult =  new Word (this.word, this.definition, this.partOfSpeech, this.origin, this.synonyms);
-      // });
     });
   }
 
